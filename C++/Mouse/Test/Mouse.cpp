eb249a67d62e18ab2f5902e442e70a82bb3a3f82
simonhess8 <hess.simon.99@gmx.de>
2021-02-12 12:33:42 +0100

    Grobe Grundstruktur

--- C++/Mouse/Test/Mouse.cpp
+++ C++/Mouse/Test/Mouse.cpp
@@ -8,22 +8,42 @@
 
 #define VK_D 0x44;
 
+short Horizontal[20] = { 2, 3, 4, -2, -1, 3, 5, 0, 2, -1, 3,
+					2, 3, 4, -2, -1, 3, 5, 0, 2 };
+
+short Vertikal[20] = { 2, -3, 8, -2, -1, 3, 9, 0, 2, -5, 3,
+					2, 3, 4, -2, -1, 2, 5, 0, 2 };
+
+void getData();
+void Move(short Hori, short Verti, short Sensitivity);
+void LeftClick();
+void RightClick();
+void ShowDesktop();
+
 int main()
 {
+	do
+	{
+		ShowDesktop();
+		for (int i = 0; i < sizeof(Horizontal) / sizeof(short); i++)
+		{
+			Move(Horizontal[i], Vertikal[i], 15);
+		}
+
+		RightClick();
+	} while (true);
 	
 }
 
-void Move(short right, short left, short up, short down)
+void Move(short Hori, short Verti, short Sensitivity)
 {
-	
+
 	INPUT inputs[1];
 	ZeroMemory(inputs, sizeof(inputs));
-	
+
 	inputs[0].type = INPUT_MOUSE;
-	inputs[0].mi.dx = right;
-	inputs[0].mi.dx = left;
-	inputs[0].mi.dy = up;
-	inputs[0].mi.dy = down;
+	inputs[0].mi.dx = Hori * Sensitivity;
+	inputs[0].mi.dy = Verti * Sensitivity;
 	inputs[0].mi.dwFlags = MOUSE_MOVED;
 
 	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
@@ -48,7 +68,20 @@ void LeftClick()
 		std::cout << L"SendInput failed: 0x" << HRESULT_FROM_WIN32(GetLastError()) << "/n";
 	}
 }
+void RightClick()
+{
+	INPUT inputs[1];
+	ZeroMemory(inputs, sizeof(inputs));
 
+	inputs[0].type = INPUT_MOUSE;
+	inputs[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+
+	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+	if (uSent != ARRAYSIZE(inputs))
+	{
+		std::cout << L"SendInput failed: 0x" << HRESULT_FROM_WIN32(GetLastError()) << "/n";
+	}
+}
 void ShowDesktop()
 {
 	INPUT inputs[4];
